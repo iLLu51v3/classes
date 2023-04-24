85af36af94288c0af2338342ee33e3a39263b18d
Jeremy Laabs <jeremy_calcium@homtail.com>
2023-04-24 18:43:18 -0500

    save 1

--- index.js
+++ index.js
@@ -44,7 +44,23 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = []
+  }
+  //methods
+  eat(edible){
+    if(this.stomach.length <10){
+      this.stomach.push(edible);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -62,7 +78,26 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon; 
+    this.tank = 0;
+    this.odometer = 0; 
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    const driveDist = this.tank * this.milesPerGallon;
+    if (distance <= driveDist){
+     this.odometer = this.odometer + distance;
+     this.tank = this.tank - distance / this.milesPerGallon
+    }else{
+      this.odometer = this.odometer + driveDist;
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
 
 /*
@@ -79,7 +114,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor(name, age, location){ 
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -97,8 +139,13 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}){
+    super();
+    this.specialty = specialty
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+  }
 }
 
 /*
